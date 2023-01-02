9b9032609a802387d6028e6a58ec74c4cc925fa4
vafominov <vafominov@avito.ru>
2023-01-02 20:57:05 +0400

    challenge 7: testing

--- challenges/testing/begin/main_test.go
+++ challenges/testing/begin/main_test.go
@@ -1,8 +1,67 @@
 // challenges/testing/begin/main_test.go
 package main
 
-// write a test for letterCounter.count
+import "testing"
 
-// write a test for numberCounter.count
+func TestLetterCount(t *testing.T) {
+	var tests = map[string]struct {
+		input string
+		want  int
+	}{
+		"empty": {input: "", want: 0},
+		"one":   {input: "a2 32 ^ &/)", want: 1},
+		"two":   {input: "812 %6ab//", want: 2},
+	}
 
-// write a test for symbolCounter.count
+	lc := letterCounter{}
+
+	for _, tc := range tests {
+		t.Run(tc.input, func(t *testing.T) {
+			if got := lc.count(tc.input); got != tc.want {
+				t.Errorf("got %v want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestNumberCount(t *testing.T) {
+	var tests = map[string]struct {
+		input string
+		want  int
+	}{
+		"empty": {input: "", want: 0},
+		"one":   {input: "abc 1,?/", want: 1},
+		"two":   {input: "abc 12&8 ^", want: 3},
+	}
+
+	nc := numberCounter{}
+
+	for _, tc := range tests {
+		t.Run(tc.input, func(t *testing.T) {
+			if got := nc.count(tc.input); got != tc.want {
+				t.Errorf("got %v want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestSymbolCount(t *testing.T) {
+	var tests = map[string]struct {
+		input string
+		want  int
+	}{
+		"empty": {input: "", want: 0},
+		"one":   {input: "abc 1,?/", want: 4},
+		"two":   {input: "abc 12&8 ^_", want: 5},
+	}
+
+	sc := symbolCounter{}
+
+	for _, tc := range tests {
+		t.Run(tc.input, func(t *testing.T) {
+			if got := sc.count(tc.input); got != tc.want {
+				t.Errorf("got %v want %v", got, tc.want)
+			}
+		})
+	}
+}
--- testing/basics/begin/main_test.go
+++ testing/basics/begin/main_test.go
@@ -1,6 +1,25 @@
 // testing/basics/begin/main_test.go
 package main
 
+import (
+	"fmt"
+	"testing"
+)
+
 // write a test for sum
+func TestSum(t *testing.T) {
+	got := sum(1, 2)
+	want := 3
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
 
 // write a TestMain for setup and teardown
+func TestMain(m *testing.M) {
+	// setup
+	fmt.Println("*********SETUP*********")
+	m.Run()
+	// teardown
+	fmt.Println("*********TEARDOWN*********")
+}
